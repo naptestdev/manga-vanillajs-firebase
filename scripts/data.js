b3e98be7a9a8c0df00dbcdb64f0a4b261cc5cbd7
naptestdev <naptestfordev@gmail.com>
2023-12-13 21:51:10 +0700

    initial commit

---
+++ scripts/data.js
@@ -0,0 +1,239 @@
+export const mangas = [
+  {
+    title: "Thanh Gươm Diệt Quỷ",
+    cover: "http://st.nettruyenus.com/data/comics/235/thanh-guom-diet-quy.jpg",
+    chapter: "Chapter 205.6",
+    id: "thanh-guom-diet-quy",
+  },
+  {
+    title: "Thám Tử Conan",
+    cover: "http://st.nettruyenus.com/data/comics/30/tham-tu-conan-7281.jpg",
+    chapter: "Chapter 1122",
+    id: "tham-tu-conan",
+  },
+  {
+    title: "Anh Hùng OnePunch",
+    cover:
+      "http://st.nettruyenus.com/data/comics/37/anh-hung-onepunch-5074.jpg",
+    chapter: "Chapter 244",
+    id: "anh-hung-onepunch",
+  },
+  {
+    title: "Kịch Trường Của Takemichi",
+    cover:
+      "http://st.nettruyenus.com/data/comics/94/kich-truong-cua-takemichi.jpg",
+    chapter: "Chapter 278.8",
+    id: "kich-truong-cua-takemichi",
+  },
+  {
+    title: "Mairimashita! Iruma-kun",
+    cover:
+      "http://st.nettruyenus.com/data/comics/113/mairimashita-iruma-kun.jpg",
+    chapter: "Chapter 328",
+    id: "mairimashita-iruma-kun",
+  },
+  {
+    title: "Đảo Hải Tặc",
+    cover: "http://st.nettruyenus.com/data/comics/209/dao-hai-tac-5452.jpg",
+    chapter: "Chapter 1101",
+    id: "dao-hai-tac",
+  },
+  {
+    title: "Kingdom - Vương Giả Thiên Hạ",
+    cover: "http://st.nettruyenus.com/data/comics/53/vuong-gia-thien-ha.jpg",
+    chapter: "Chapter 779",
+    id: "kingdom-vuong-gia-thien-ha",
+  },
+  {
+    title: "Vĩnh Hằng Chí Tôn",
+    cover: "http://st.nettruyenus.com/data/comics/101/vinh-hang-chi-ton.jpg",
+    chapter: "Chapter 326",
+    id: "vinh-hang-chi-ton",
+  },
+  {
+    title: "Chú Thuật Hồi Chiến",
+    cover: "http://st.nettruyenus.com/data/comics/42/chu-thuat-hoi-chien.jpg",
+    chapter: "Chapter 244",
+    id: "chu-thuat-hoi-chien",
+  },
+  {
+    title: "Trường Học Siêu Anh Hùng",
+    cover:
+      "http://st.nettruyenus.com/data/comics/182/truong-hoc-sieu-anh-hung-3626.jpg",
+    chapter: "Chapter 409",
+    id: "truong-hoc-sieu-anh-hung",
+  },
+  {
+    title: "Black Clover - Pháp sư không phép thuật",
+    cover:
+      "http://st.nettruyenus.com/data/comics/69/black-clover-phap-su-khong-phep-thuat-6382.jpg",
+    chapter: "Chapter 368",
+    id: "black-clover-phap-su-khong-phep-thuat",
+  },
+  {
+    title: "Tôi Đã Chuyển Sinh Thành Slime",
+    cover:
+      "http://st.nettruyenus.com/data/comics/78/toi-da-chuyen-sinh-thanh-slime.jpg",
+    chapter: "Chapter 113",
+    id: "toi-da-chuyen-sinh-thanh-slime",
+  },
+  {
+    title: "Shuumatsu no Valkyrie",
+    cover:
+      "http://st.nettruyenus.com/data/comics/247/shuumatsu-no-valkyrie.jpg",
+    chapter: "Chapter 84.5",
+    id: "shuumatsu-no-valkyrie",
+  },
+  {
+    title: "Mashle: Muscles and Magic",
+    cover:
+      "http://st.nettruyenus.com/data/comics/120/mashle-muscles-and-magic.jpg",
+    chapter: "Chapter 162",
+    id: "mashle-muscles-and-magic",
+  },
+  {
+    title: "Chainsaw man - Thợ Săn Quỷ",
+    cover:
+      "http://st.nettruyenus.com/data/comics/41/chainsaw-man-tho-san-quy-5805.jpg",
+    chapter: "Chapter 150",
+    id: "chainsaw-man-tho-san-quy",
+  },
+  {
+    title: "Tiền đạo số 1",
+    cover: "http://st.nettruyenus.com/data/comics/189/tien-dao-so-1.jpg",
+    chapter: "Chapter 244",
+    id: "tien-dao-so-1",
+  },
+  {
+    title: "Dr.Stone - Hồi Sinh Thế Giới",
+    cover:
+      "http://st.nettruyenus.com/data/comics/236/drstone-hoi-sinh-the-gioi-8635.jpg",
+    chapter: "Chapter 232.6",
+    id: "drstone-hoi-sinh-the-gioi",
+  },
+  {
+    title: "Gia Đình Điệp Viên",
+    cover:
+      "http://st.nettruyenus.com/data/comics/162/gia-dinh-diep-vien-2317.jpg",
+    chapter: "Chapter 91",
+    id: "gia-dinh-diep-vien",
+  },
+  {
+    title: "Hội Pháp Sư nhiệm vụ trăm năm",
+    cover:
+      "http://st.nettruyenus.com/data/comics/173/fairy-tail-100-year-quest.jpg",
+    chapter: "Chapter 146",
+    id: "hoi-phap-su-nhiem-vu-tram-nam",
+  },
+  {
+    title: "Chuyện Của Hori Và Miyamura",
+    cover:
+      "http://st.nettruyenus.com/data/comics/13/chuyen-cua-hori-va-miyamura.jpg",
+    chapter: "Chapter 122.8",
+    id: "chuyen-cua-hori-va-miyamura",
+  },
+  {
+    title: "Ngũ Đại Đồ Đệ Của Charlotte",
+    cover:
+      "http://st.nettruyenus.com/data/comics/189/ngu-dai-do-de-cua-charlotte.jpg",
+    chapter: "Chapter 196.2",
+    id: "ngu-dai-do-de-cua-charlotte",
+  },
+  {
+    title: "Naruto - Cửu Vĩ Hồ Ly",
+    cover: "http://st.nettruyenus.com/data/comics/220/naruto-cuu-vi-ho-ly.jpg",
+    chapter: "Chapter 700.9",
+    id: "naruto-cuu-vi-ho-ly",
+  },
+  {
+    title: "Oshi no Ko",
+    cover: "http://st.nettruyenus.com/data/comics/178/oshi-no-ko.jpg",
+    chapter: "Chapter 133",
+    id: "oshi-no-ko",
+  },
+  {
+    title: "Đối Phương Phải Tỏ Tình Trước",
+    cover:
+      "http://st.nettruyenus.com/data/comics/25/doi-phuong-phai-to-tinh-truoc-7476.jpg",
+    chapter: "Chapter 281.7",
+    id: "doi-phuong-phai-to-tinh-truoc",
+  },
+  {
+    title: "Tao muốn trở thành chúa tể bóng tối!!",
+    cover:
+      "http://st.nettruyenus.com/data/comics/228/tao-muon-tro-thanh-chua-te-bong-toi-6263.jpg",
+    chapter: "Chapter 59",
+    id: "tao-muon-tro-thanh-chua-te-bong-toi",
+  },
+  {
+    title: "Kích Trụy Vương huyền thoại",
+    cover:
+      "http://st.nettruyenus.com/data/comics/116/kich-truy-vuong-huyen-thoai.jpg",
+    chapter: "Chapter 82",
+    id: "kich-truy-vuong-huyen-thoai",
+  },
+  {
+    title: "Thế Giới Tiên Hiệp",
+    cover: "http://st.nettruyenus.com/data/comics/192/the-gioi-tien-hiep.jpg",
+    chapter: "Chapter 207",
+    id: "the-gioi-tien-hiep",
+  },
+  {
+    title: "Sống Lại Tuổi 20",
+    cover: "http://st.nettruyenus.com/data/comics/71/song-lai-tuoi-20-1449.jpg",
+    chapter: "Chapter 177",
+    id: "song-lai-tuoi-20",
+  },
+  {
+    title: "Đấu Trường Ngầm",
+    cover: "http://st.nettruyenus.com/data/comics/88/dau-truong-ngam.jpg",
+    chapter: "Chapter 238",
+    id: "dau-truong-ngam",
+  },
+  {
+    title: "Tóm lại là em dễ thương được chưa ?",
+    cover:
+      "http://st.nettruyenus.com/data/comics/219/tom-lai-la-em-de-thuong-duoc-chua.jpg",
+    chapter: "Chapter 255",
+    id: "tom-lai-la-em-de-thuong-duoc-chua",
+  },
+  {
+    title: "Quái Vật #8",
+    cover: "http://st.nettruyenus.com/data/comics/64/quai-vat-8.jpg",
+    chapter: "Chapter 97",
+    id: "quai-vat-8",
+  },
+  {
+    title: "Đấm phát chết luôn (Gốc)",
+    cover: "http://st.nettruyenus.com/data/comics/192/onepunch-man-one.jpg",
+    chapter: "Chapter 146",
+    id: "dam-phat-chet-luon-goc",
+  },
+  {
+    title: "Sát thủ về vườn",
+    cover: "http://st.nettruyenus.com/data/comics/227/sat-thu-ve-vuon-7939.jpg",
+    chapter: "Chapter 147",
+    id: "sat-thu-ve-vuon",
+  },
+  {
+    title: "The Five Wedded Brides",
+    cover:
+      "http://st.nettruyenus.com/data/comics/62/the-five-wedded-brides.jpg",
+    chapter: "Chapter 122.6",
+    id: "the-five-wedded-brides",
+  },
+  {
+    title: "Cuộc Sống Thong Dong Ở Dị Thế Giới",
+    cover:
+      "http://st.nettruyenus.com/data/comics/44/cuoc-song-thong-dong-o-di-the-gioi.jpg",
+    chapter: "Chapter 148",
+    id: "cuoc-song-thong-dong-o-di-the-gioi",
+  },
+  {
+    title: "Tsuki ga Michibiku Isekai Douchuu",
+    cover:
+      "http://st.nettruyenus.com/data/comics/209/tsuki-ga-michibiku-isekai-douchuu.jpg",
+    chapter: "Chapter 89",
+    id: "tsuki-ga-michibiku-isekai-douchuu",
+  },
+];
